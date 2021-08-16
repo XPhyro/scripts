b69a3efbd816e9f76be45b57b8b16c905cfb156e
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2021-08-16 22:24:10 +0300

    Create c/fputs and c/puts

---
+++ src/c/fputs/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int i;
+
+    if (argc < 2) {
+        fputs("At least 1 argument is needed, exiting.\n", stderr);
+        return 1;
+    }
+
+    for (i = 0; i < argc - 1; i++) {
+        fputs(argv[i], stdout);
+        fputs(" ", stdout);
+    }
+    fputs(argv[argc - 1], stdout);
+
+    return 0;
+}
---
+++ src/c/puts/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int i;
+
+    if (argc < 2) {
+        fputs("At least 1 argument is needed, exiting.\n", stderr);
+        return 1;
+    }
+
+    for (i = 1; i < argc; i++) {
+        puts(argv[i]);
+    }
+
+    return 0;
+}
