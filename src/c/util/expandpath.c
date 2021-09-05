d9e064b22278c93621295772076f9c66b06fd821
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2021-09-05 18:46:01 +0300

    Rewrite expandpath in C

---
+++ src/c/util/expandpath.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <wordexp.h>
+
+int main(int argc, char *argv[])
+{
+    int i;
+    char *line = NULL;
+    size_t size;
+    wordexp_t result;
+
+    if (argc != 1) {
+        for (i = 1; i < argc; i++) {
+            wordexp(argv[i], &result, 0);
+            printf("%s\n", result.we_wordv[0]);
+            wordfree(&result);
+        }
+    } else {
+        while (getline(&line, &size, stdin) != -1) {
+            line[strlen(line) - 1] = '\0';
+            wordexp(line, &result, 0);
+            printf("%s\n", result.we_wordv[0]);
+            wordfree(&result);
+        }
+    }
+
+    return 0;
+}
