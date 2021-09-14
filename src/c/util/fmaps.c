3c7e2d680bb27288f398882a5bb9ca9f1089ec52
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2021-09-14 07:04:54 +0300

    Rewrite fmaps in C

---
+++ src/c/util/fmaps.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *strnext(char *s)
+{
+    char *scp;
+    scp = s;
+    while(*(scp++));
+    return scp;
+}
+
+int main(int argc, char *argv[])
+{
+    char *sep, *end, *line, *eol;
+    char c;
+    int i;
+    size_t len;
+    ssize_t nread;
+
+    end = getenv("END");
+    if (!end)
+        end = "\n";
+
+    if (argc == 1) {
+        while((c = getchar()) != EOF) {
+            if (c != '\n')
+                putchar(c);
+            else
+                fputs(end, stdout);
+        }
+        return 0;
+    }
+
+    sep = getenv("SEP");
+    if (!sep)
+        sep = "=";
+
+    for (i = 1; i < argc; i++)
+        argv[i] = strtok(argv[i], sep);
+
+    line = NULL;
+    len = 0;
+    while ((nread = getline(&line, &len, stdin)) != -1) {
+        eol = strnext(line) - 2;
+        if (*eol == '\n')
+            *eol = '\0';
+        for (i = 1; i < argc; i++) {
+            if (strcmp(line, argv[i]) == 0) {
+                printf("%s%s", strnext(argv[i]), end);
+                goto newline;
+            }
+        }
+        printf("%s%s", line, end);
+newline:
+    }
+    
+    return 0;
+}
