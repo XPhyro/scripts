2fbded5117c230d4ef4e3cbac5a422b6b59f804a
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2021-09-26 16:39:44 +0300

    Add file support in shufr

--- src/c/util/shufr.c
+++ src/c/util/shufr.c
@@ -4,23 +4,38 @@
 #include <stdlib.h>
 #include <memory.h>
 #include <time.h>
+#include <unistd.h>
+
+#define SETLINES(stream) while ((len = getdelim(&line, &size, delim, stream)) != -1) { \
+                             tmpline = malloc((len - 1) * sizeof(char)); \
+                             memcpy(tmpline, line, len - 1); \
+                             lines[i++] = tmpline; \
+                             if (i == n) \
+                                 lines = realloc(line, (n *= 2) * sizeof(char *)); \
+                         }
 
 int main(int argc, char *argv[])
 {
-    int n = 100, i, tmpi;
+    int n = 100, i, tmpi, nfile, j, nsame;
     const char delim = '\0';
     char *line = NULL, *tmpline = NULL;
-    char **lines = malloc(n * sizeof(char *));
+    char **lines = malloc(n * sizeof(char *)), **files;
     size_t size;
     ssize_t len;
+    FILE *file;
+
+    while ((i = getopt(argc, argv, "")) != -1);
+
+    files = argv + optind;
+    nfile = argc - optind;
 
     i = 0;
-    while ((len = getdelim(&line, &size, delim, stdin)) != -1) {
-        tmpline = malloc((len - 1) * sizeof(char));
-        memcpy(tmpline, line, len - 1);
-        lines[i++] = tmpline;
-        if (i == n)
-            lines = realloc(line, (n *= 2) * sizeof(char *));
+    if (nfile == 0) {
+        SETLINES(stdin);
+    } else for (j = 0; j < nfile; j++) {
+        file = fopen(files[j], "r");
+        SETLINES(file);
+        fclose(file);
     }
 
     lines = realloc(lines, (n = i) * sizeof(char *));
