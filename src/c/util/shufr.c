5ea2389921bcf39fea1b84f56b9eb3d74d629a97
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2021-09-26 17:21:15 +0300

    Implement -lnz0 options in shufr

--- src/c/util/shufr.c
+++ src/c/util/shufr.c
@@ -5,26 +5,60 @@
 #include <memory.h>
 #include <time.h>
 #include <unistd.h>
+#include <errno.h>
 
 #define SETLINES(stream) while ((len = getdelim(&line, &size, delim, stream)) != -1) { \
-                             tmpline = malloc((len - 1) * sizeof(char)); \
-                             memcpy(tmpline, line, len - 1); \
-                             lines[i++] = tmpline; \
+                             tmpstr = malloc((len - 1) * sizeof(char)); \
+                             memcpy(tmpstr, line, len - 1); \
+                             lines[i++] = tmpstr; \
                              if (i == n) \
                                  lines = realloc(line, (n *= 2) * sizeof(char *)); \
                          }
 
 int main(int argc, char *argv[])
 {
-    int n = 100, i, tmpi, nfile, j, nsame;
-    const char delim = '\0';
-    char *line = NULL, *tmpline = NULL;
+    int n = 100, nfile, i, j, k;
+    int *ihist, *iarr;
+    unsigned int lprint = 0, nprint = 0, nsame = 1, nsbuf = 2;
+    char delim = '\n';
+    char *line = NULL, *tmpstr = NULL;
     char **lines = malloc(n * sizeof(char *)), **files;
     size_t size;
     ssize_t len;
     FILE *file;
 
-    while ((i = getopt(argc, argv, "")) != -1);
+    while ((i = getopt(argc, argv, "l:n:z0")) != -1) {
+        switch (i) {
+            case 'l':
+                errno = 0;
+                lprint = strtoul(optarg, &tmpstr, 10);
+                if (errno) {
+                    perror("strtoul");
+                    exit(EXIT_FAILURE);
+                }
+                if (optarg == tmpstr) {
+                    fputs("ERROR[shufr]: Invalid number given to option -l.\n", stderr);
+                    exit(EXIT_FAILURE);
+                }
+                break;
+            case 'n':
+                errno = 0;
+                nsbuf = 2 * (nsame = strtoul(optarg, &tmpstr, 10));
+                if (errno) {
+                    perror("strtoul");
+                    exit(EXIT_FAILURE);
+                }
+                if (optarg == tmpstr) {
+                    fputs("ERROR[shufr]: Invalid number given to option -n.\n", stderr);
+                    exit(EXIT_FAILURE);
+                }
+                break;
+            case 'z':
+            case '0':
+                delim = '\0';
+                break;
+        }
+    }
 
     files = argv + optind;
     nfile = argc - optind;
@@ -39,14 +73,32 @@ int main(int argc, char *argv[])
     }
 
     lines = realloc(lines, (n = i) * sizeof(char *));
+    ihist = malloc(nsbuf * sizeof(int));
+    iarr  = malloc(nsame * sizeof(int));
+
+    for (i = 0; i < nsbuf; i++) {
+        ihist[i] = -1;
+    }
 
     srand(time(NULL));
     for (;;) {
-        tmpi = rand() % n;
-        if (tmpi == i)
-            continue;
-        i = tmpi;
-        printf("%s%c", lines[i], delim);
+newcycle:
+        for (i = 0; i < nsame; i++) {
+            k = rand() % n;
+            for (j = 0; j < nsbuf; ++j) {
+                if (ihist[j] == k)
+                    goto newcycle;
+            }
+            ihist[nsame + i] = iarr[i] = k;
+        }
+        for (i = 0; i < nsame; i++) {
+            printf("%s%c", lines[ihist[i] = iarr[i]], delim);
+            if (lprint && ++nprint == lprint)
+                exit(EXIT_SUCCESS);
+        }
+        for (i = nsame; i < nsbuf; i++) {
+            ihist[i] = -1;
+        }
     }
 
     return 0;
