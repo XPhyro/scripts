25909abad131775fd89378dae44a9f53c3689504
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2021-10-07 14:21:41 +0300

    Refactor c/util/eval-verbose.c

--- src/c/util/eval-verbose.c
+++ src/c/util/eval-verbose.c
@@ -7,16 +7,9 @@ int main(int argc, char *argv[])
     char *s, *endptr;
     int n;
 
-    if (!(s = getenv("SHELL_VERBOSE")))
-        return 0;
-
     errno = 0;
-    n = strtol(s, &endptr, 10);
-
-    if (errno || s == endptr || n <= 0)
-        return 0;
-
-    puts("set -x");
+    if ((s = getenv("SHELL_VERBOSE")) && (n = strtol(s, &endptr, 10)) > 0 && !errno && s != endptr)
+        puts("set -x");
 
     return 0;
 }
