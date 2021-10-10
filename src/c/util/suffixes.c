f9dacc79476214ae71579a9e05c4a4ecf97d4b55
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2021-10-10 13:37:46 +0300

    Rewrite suffixes in C

---
+++ src/c/util/suffixes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#include "../include/strutil.h"
+
+int main(int argc, char *argv[])
+{
+    int i;
+
+    for (i = 2; i < argc; i++)
+        if (strsfx(argv[1], argv[i]))
+            return 0;
+
+    return 1;
+}
