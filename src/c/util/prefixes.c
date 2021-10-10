e3a3827594510f1e6b8c77da70ffab7baf212512
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2021-10-10 12:56:25 +0300

    Rewrite prefixes in C

---
+++ src/c/util/prefixes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#include "../include/strutil.h"
+
+int main(int argc, char *argv[])
+{
+    int i;
+
+    for (i = 2; i < argc; i++)
+        if (strpfx(argv[1], argv[i]))
+            return 0;
+
+    return 1;
+}
