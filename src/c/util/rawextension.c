42cf8282fadd3e381c89b95b49a97ae657b76a6a
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2021-10-25 00:32:54 +0300

    Rewrite raw{extension,name} in C

---
+++ src/c/util/rawextension.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    int i;
+    char *s;
+
+    for (i = 1; i < argc; i++) {
+        if (!(s = strrchr(argv[i], '.'))) {
+            puts(argv[i]);
+        } else {
+            *s = '\0';
+            puts(s + 1);
+        }
+    }
+
+    return 0;
+}
---
+++ src/c/util/rawname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    int i;
+    char *s;
+
+    for (i = 1; i < argc; i++) {
+        if ((s = strrchr(argv[i], '.')))
+            *s = '\0';
+        puts(argv[i]);
+    }
+
+    return 0;
+}
