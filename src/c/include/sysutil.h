579c24e809c037086c32253a524a8b9079bb6508
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2021-10-25 06:16:04 +0300

    Refactor and format c/include/sysutil.h

--- src/c/include/sysutil.h
+++ src/c/include/sysutil.h
@@ -6,9 +6,9 @@
 #include <sys/stat.h>
 
 /* path is modified in the process, but restored to original */
-void rmkfile(char *path, mode_t mode)
+void rmkparent(char *path, mode_t mode)
 {
-    size_t size = strlen(path) + 1, i, j;
+    size_t i, j, size = strlen(path) + 1;
 
     for (i = 1, j = 0; i < size; i++) {
         if (path[i] == '/') {
@@ -20,40 +20,18 @@ void rmkfile(char *path, mode_t mode)
     }
     if (j)
         path[j] = '/';
+}
+
+/* path is modified in the process, but restored to original */
+void rmkfile(char *path, mode_t mode)
+{
+    rmkparent(path, mode);
     fopen(path, "a");
 }
 
 /* path is modified in the process, but restored to original */
 void rmkdir(char *path, mode_t mode)
 {
-    size_t size = strlen(path) + 1, i, j;
-
-    for (i = 1, j = 0; i < size; i++) {
-        if (path[i] == '/') {
-            if (j)
-                path[j] = '/';
-            path[j = i] = '\0';
-            mkdir(path, mode);
-        }
-    }
-    if (j)
-        path[j] = '/';
+    rmkparent(path, mode);
     mkdir(path, mode);
-}
-
-/* path is modified in the process, but restored to original */
-void rmkparent(char *path, mode_t mode)
-{
-    size_t size = strlen(path) + 1, i, j;
-
-    for (i = 1, j = 0; i < size; i++) {
-        if (path[i] == '/') {
-            if (j)
-                path[j] = '/';
-            path[j = i] = '\0';
-            mkdir(path, mode);
-        }
-    }
-    if (j)
-        path[j] = '/';
 }
