539fb64692866b34804f8a862f1bdf3f174bb061
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2021-11-13 17:45:14 +0300

    Remove ones and zeros, create repeat{line,null,str}

---
+++ src/c/util/repeatstr.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int i, l = argc - 1;
+
+    if (argc == 1)
+        return 0;
+
+    for (;;) {
+        for (i = 1; i < l; i++) {
+            fputs(argv[i], stdout);
+            putchar(' ');
+        }
+        fputs(argv[l], stdout);
+    }
+}
