c4979579fa72aea5e7cf97b62911177b902dbc27
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2022-01-30 13:39:45 +0300

    Add -o option and cbrt & pow functions in numsh

--- src/c/util/math/numsh.c
+++ src/c/util/math/numsh.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <unistd.h>
 #include <math.h>
 
@@ -10,13 +11,10 @@
 
 #define EXECNAME "numsh"
 
-#define DIE(ERRMSG) { fputs(EXECNAME": "ERRMSG"\n", stderr); \
-                      exit(EXIT_FAILURE); }
-
 #define LEN(ARR) (sizeof(ARR) / sizeof((ARR)[0]))
 
-#define DECLMAP(FUNC) double map_##FUNC(int, double *)
-#define DECLREDUCE(FUNC) double reduce_##FUNC(int, double *)
+#define DECLMAP(FUNC) double map_##FUNC(int, double *, double *)
+#define DECLREDUCE(FUNC) double reduce_##FUNC(int, double *, double *)
 
 #define FUNCMAP(FUNC) { #FUNC, map_##FUNC, FUNCTYPE_MAP }
 #define FUNCREDUCE(FUNC) { #FUNC, reduce_##FUNC, FUNCTYPE_REDUCE }
@@ -28,13 +26,14 @@ typedef enum {
 
 typedef struct function {
     char *name;
-    double (*func)(int, double *);
+    double (*func)(int, double *, double *);
     FUNCTYPE type;
 } function;
 
 DECLMAP(acos);
 DECLMAP(asin);
 DECLMAP(atan);
+DECLMAP(cbrt);
 DECLMAP(ceil);
 DECLMAP(cos);
 DECLMAP(cosh);
@@ -44,6 +43,7 @@ DECLMAP(floor);
 DECLMAP(log);
 DECLMAP(log10);
 DECLMAP(log2);
+DECLMAP(pow);
 DECLMAP(round);
 DECLMAP(sin);
 DECLMAP(sinh);
@@ -60,6 +60,7 @@ function functions[] = {
     FUNCMAP(acos),
     FUNCMAP(asin),
     FUNCMAP(atan),
+    FUNCMAP(cbrt),
     FUNCMAP(ceil),
     FUNCMAP(cos),
     FUNCMAP(cosh),
@@ -69,6 +70,7 @@ function functions[] = {
     FUNCMAP(log),
     FUNCMAP(log10),
     FUNCMAP(log2),
+    FUNCMAP(pow),
     FUNCMAP(round),
     FUNCMAP(sin),
     FUNCMAP(sinh),
@@ -82,16 +84,31 @@ function functions[] = {
     FUNCREDUCE(sum),
 };
 
+void die(const char *fmt, ...)
+{
+    va_list ap;
+
+    fputs(EXECNAME": ", stderr);
+
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    va_end(ap);
+
+    putchar('\n');
+
+    exit(EXIT_FAILURE);
+}
+
 int main(int argc, char *argv[])
 {
     int i, j;
     char *line = NULL;
-    size_t size;
+    size_t size, funckwargc = 0;
     ssize_t len;
     function *func = NULL;
-    double *nums;
+    double *funcargv, *funckwargv = NULL;
 
-    while ((i = getopt(argc, argv, "f:hL")) != -1) {
+    while ((i = getopt(argc, argv, "f:hLo:")) != -1) {
         switch (i) {
             case 'f': /* TODO: support multiple functions */
                 for (j = 0; j < LEN(functions); j++) {
@@ -100,19 +117,25 @@ int main(int argc, char *argv[])
                         goto funcavail;
                     }
                 }
-                DIE("invalid function given")
+                die("invalid function given");
 funcavail:
                 break;
             case 'h':
-                puts("Usage: "EXECNAME" [OPTION]... [NUMBER]...\n"
+                fputs("Usage: "EXECNAME" [OPTION]... [NUMBER]...\n"
                      "Do mathematical calculations on all given numbers.\n"
                      "\n"
                      "With no NUMBER, read standard input. Empty lines are ignored\n"
                      "when reading standard input.\n"
                      "\n"
+                     "Option -f must be given. If FUNC requires additional arguments,\n"
+                     "option -o must be given exactly as many times as required.\n" /* TODO: enforce this */
+                     "\n"
                      "  -f  FUNC  function to pass numbers through. pass -L to see the list.\n"
                      "  -h        display this help and exit\n"
-                     "  -L        list all supported functions and exit");
+                     "  -L        list all supported functions and exit\n"
+                     "  -o  ARG   pass additional arguments to FUNC. this option can be\n"
+                     "            given multiple times, and has to be given after -f.\n"
+                , stdout);
                 exit(EXIT_SUCCESS);
                 break;
             case 'L':
@@ -134,6 +157,11 @@ funcavail:
 
                 exit(EXIT_SUCCESS);
                 break;
+            case 'o':
+                if (!(funckwargv = realloc(funckwargv, ++funckwargc * sizeof(double))))
+                    die("could not allocate memory");
+                funckwargv[funckwargc - 1] = astrtod(optarg, EXECNAME": invalid number given\n");
+                break;
             default:
                 fputs("Try '"EXECNAME" -h' for more information.\n", stderr);
                 exit(EXIT_FAILURE);
@@ -142,136 +170,146 @@ funcavail:
     }
 
     if (!func)
-        DIE("no function given");
+        die("no function given");
 
     argv += optind;
     argc -= optind;
 
     if (!argc) {
-        DIE("stdin is not yet supported");
+        die("stdin is not yet supported");
         while ((len = getline(&line, &size, stdin)) != -1) {
             if (!len)
                 continue;
             if (line[len - 1] == '\n')
                 line[len - 1] = '\0';
-            /* TODO: build `nums` */
+            /* TODO: build `args` */
         }
     } else {
-        nums = malloc(sizeof(double) * argc);
+        funcargv = malloc(sizeof(double) * argc);
         for (i = 0; i < argc; i++)
-            nums[i] = astrtod(argv[i], EXECNAME": invalid number given\n");
+            funcargv[i] = astrtod(argv[i], EXECNAME": invalid number given\n");
     }
 
     switch (func->type) {
         case FUNCTYPE_MAP:
             for (i = 0; i < argc; i++) {
-                printf("%.16g\n", func->func(1, nums + i));
+                printf("%.16g\n", func->func(1, funcargv + i, funckwargv));
             }
             break;
         case FUNCTYPE_REDUCE:
-            printf("%.16g\n", func->func(argc, nums));
+            printf("%.16g\n", func->func(argc, funcargv, funckwargv));
             break;
     }
 
     return 0;
 }
 
-double map_acos(int argc, double *argv)
+double map_acos(int argc, double *argv, double *kwargv)
 {
     return acos(argv[0]);
 }
 
-double map_asin(int argc, double *argv)
+double map_asin(int argc, double *argv, double *kwargv)
 {
     return asin(argv[0]);
 }
 
-double map_atan(int argc, double *argv)
+double map_atan(int argc, double *argv, double *kwargv)
 {
     return atan(argv[0]);
 }
 
-double map_ceil(int argc, double *argv)
+double map_cbrt(int argc, double *argv, double *kwargv)
+{
+    return cbrt(argv[0]);
+}
+
+double map_ceil(int argc, double *argv, double *kwargv)
 {
     return ceil(argv[0]);
 }
 
-double map_cos(int argc, double *argv)
+double map_cos(int argc, double *argv, double *kwargv)
 {
     return cos(argv[0]);
 }
 
-double map_cosh(int argc, double *argv)
+double map_cosh(int argc, double *argv, double *kwargv)
 {
     return cosh(argv[0]);
 }
 
-double map_exp(int argc, double *argv)
+double map_exp(int argc, double *argv, double *kwargv)
 {
     return exp(argv[0]);
 }
 
-double map_fabs(int argc, double *argv)
+double map_fabs(int argc, double *argv, double *kwargv)
 {
     return fabs(argv[0]);
 }
 
-double map_floor(int argc, double *argv)
+double map_floor(int argc, double *argv, double *kwargv)
 {
     return floor(argv[0]);
 }
 
-double map_log(int argc, double *argv)
+double map_log(int argc, double *argv, double *kwargv)
 {
     return log(argv[0]);
 }
 
-double map_log10(int argc, double *argv)
+double map_log10(int argc, double *argv, double *kwargv)
 {
     return log10(argv[0]);
 }
 
-double map_log2(int argc, double *argv)
+double map_log2(int argc, double *argv, double *kwargv)
 {
     return log2(argv[0]);
 }
 
-double map_round(int argc, double *argv)
+double map_pow(int argc, double *argv, double *kwargv)
+{
+    return pow(argv[0], kwargv[0]);
+}
+
+double map_round(int argc, double *argv, double *kwargv)
 {
     return round(argv[0]);
 }
 
-double map_sin(int argc, double *argv)
+double map_sin(int argc, double *argv, double *kwargv)
 {
     return sin(argv[0]);
 }
 
-double map_sinh(int argc, double *argv)
+double map_sinh(int argc, double *argv, double *kwargv)
 {
     return sinh(argv[0]);
 }
 
-double map_sqrt(int argc, double *argv)
+double map_sqrt(int argc, double *argv, double *kwargv)
 {
     return sqrt(argv[0]);
 }
 
-double map_tan(int argc, double *argv)
+double map_tan(int argc, double *argv, double *kwargv)
 {
     return tan(argv[0]);
 }
 
-double map_tanh(int argc, double *argv)
+double map_tanh(int argc, double *argv, double *kwargv)
 {
     return tanh(argv[0]);
 }
 
-double map_trunc(int argc, double *argv)
+double map_trunc(int argc, double *argv, double *kwargv)
 {
     return trunc(argv[0]);
 }
 
-double reduce_min(int argc, double *argv)
+double reduce_min(int argc, double *argv, double *kwargv)
 {
     int i;
     double m = INFINITY;
@@ -284,7 +322,7 @@ double reduce_min(int argc, double *argv)
     return m;
 }
 
-double reduce_max(int argc, double *argv)
+double reduce_max(int argc, double *argv, double *kwargv)
 {
     int i;
     double m = -INFINITY;
@@ -297,7 +335,7 @@ double reduce_max(int argc, double *argv)
     return m;
 }
 
-double reduce_sum(int argc, double *argv)
+double reduce_sum(int argc, double *argv, double *kwargv)
 {
     int i;
     double s = 0;
