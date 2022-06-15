2995d69a33eae51e43037b392337f703b6a1d995
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2022-06-15 04:13:05 +0300

    Update flags in YCM configs

--- src/c/.ycm_extra_conf.py
+++ src/c/.ycm_extra_conf.py
@@ -1,20 +1,34 @@
-flags = [
-    "-O3",
-    "-std=c99",
-    "-pedantic",
-    "-Wall",
-]
+flags =
+    [
+        "-O3",
+        "-std=c99",
+        "-pedantic",
+        "-Wall",
+        "-Wextra",
+        "-Werror",
+        "-Wno-unused-parameter",
+        "-Wno-implicit-fallthrough",
+        "-Wno-sign-compare",
+        "-Wfloat-equal",
+        "-Wdouble-promotion",
+        "-Wjump-misses-init",
+        "-Wold-style-definition",
+        "-Winline",
+        "-Wpadded",
+        "-Wpacked",
+        "-Wdisabled-optimization",
+        "-lm",
+    ]
 
-SOURCE_EXTENSIONS = [
-    ".c",
-    ".h",
-]
+    SOURCE_EXTENSIONS =
+        [
+            ".c",
+            ".h",
+        ]
 
+    def
+    FlagsForFile(filename, **kwargs)
+    : return { "flags": flags, "do_cache": True }
 
-def FlagsForFile(filename, **kwargs):
-    return {"flags": flags, "do_cache": True}
-
-
-def Settings(**kwargs):
-    filename = kwargs.pop("filename")
-    return FlagsForFile(filename, **kwargs)
+    def Settings(**kwargs)
+    : filename = kwargs.pop("filename") return FlagsForFile(filename, **kwargs)
--- src/cpp/.ycm_extra_conf.py
+++ src/cpp/.ycm_extra_conf.py
@@ -1,19 +1,27 @@
-flags = [
-    "-O3",
-    "-std=c++20",  # YCM (or some other component) does not support c++23 yet and c++2b does not work
-    "-Wall",
-]
+flags =
+    [
+        "-O3",
+        "-std=c++20",
+        #YCM(or some other component) does not support c++ 23 yet and c++ 2b does not work "-Wall",
+        "-Wextra",
+        "-Werror",
+        "-Wno-unused-parameter",
+        "-Wno-implicit-fallthrough",
+        "-Wno-sign-compare",
+        "-Wfloat-equal",
+        "-Wdouble-promotion",
+        "-Wdisabled-optimization",
+    ]
 
-SOURCE_EXTENSIONS = [
-    ".cpp",
-    ".hpp",
-]
+    SOURCE_EXTENSIONS =
+        [
+            ".cpp",
+            ".hpp",
+        ]
 
+    def
+    FlagsForFile(filename, **kwargs)
+    : return { "flags": flags, "do_cache": True }
 
-def FlagsForFile(filename, **kwargs):
-    return {"flags": flags, "do_cache": True}
-
-
-def Settings(**kwargs):
-    filename = kwargs.pop("filename")
-    return FlagsForFile(filename, **kwargs)
+    def Settings(**kwargs)
+    : filename = kwargs.pop("filename") return FlagsForFile(filename, **kwargs)
