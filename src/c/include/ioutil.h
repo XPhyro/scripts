3e614084b136fd242caf55f3b39a8b2a90631b63
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2022-07-17 03:28:19 +0300

    Implement fgetstr function in ioutil header

--- src/c/include/ioutil.h
+++ src/c/include/ioutil.h
@@ -33,4 +33,19 @@ char *getstr(int argc, char *argv[], int delim)
     return NULL;
 }
 
+char *fgetstr(FILE *stream, int delim)
+{
+    static char *line = NULL;
+    static size_t size = 0;
+    ssize_t len;
+
+    if ((len = getdelim(&line, &size, delim, stream)) != -1) {
+        if (len && line[len - 1] == delim)
+            line[len - 1] = '\0';
+        return line;
+    }
+
+    return NULL;
+}
+
 #endif /* ifndef HEADER_SCRIPTS_IOUTIL */
