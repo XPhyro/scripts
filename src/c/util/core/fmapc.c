d7a4d5d600f4d9fe1baab7e8fd93b4a389b4eeec
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2022-07-27 18:26:21 +0300

    Add -b option in fmapc

--- src/c/util/core/fmapc.c
+++ src/c/util/core/fmapc.c
@@ -1,5 +1,6 @@
 #define _POSIX_C_SOURCE 200809L
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,24 +15,42 @@
         exit(EXIT_FAILURE);                   \
     }
 
+typedef struct {
+    unsigned char key;
+    unsigned char val;
+} binmapping_t;
+
 int main(int argc, char *argv[])
 {
     const char sep = '=';
-    enum { bufsize = 4096 };
+    enum { binsize = 16, bufsize = 4096 };
+    const binmapping_t binmappings[binsize] = {
+        { .key = '0', .val = 0 },  { .key = '1', .val = 1 },  { .key = '2', .val = 2 },
+        { .key = '3', .val = 3 },  { .key = '4', .val = 4 },  { .key = '5', .val = 5 },
+        { .key = '6', .val = 6 },  { .key = '7', .val = 7 },  { .key = '8', .val = 8 },
+        { .key = '9', .val = 9 },  { .key = 'a', .val = 10 }, { .key = 'b', .val = 11 },
+        { .key = 'c', .val = 12 }, { .key = 'd', .val = 13 }, { .key = 'e', .val = 14 },
+        { .key = 'f', .val = 15 },
+    };
     unsigned char buf[bufsize];
     char *s;
-    char c, p;
-    int i;
+    unsigned char c, p, bin;
+    int i, binoffset;
     size_t len;
     ssize_t nread, n;
+    bool optbin = false;
 
-    while ((i = getopt(argc, argv, "h")) != -1) {
+    while ((i = getopt(argc, argv, "bh")) != -1) {
         switch (i) {
+            case 'b':
+                optbin = true;
+                break;
             case 'h':
                 puts(
                     "Usage: fmapc [OPTION]... [MAPPING]...\n"
                     "Map standard input according to the given mappings. A MAPPING is in the form [CHAR]=[STR].\n"
                     "\n"
+                    "  -b        map hexadecimal characters to 4-bit binary and ignore mappings. non-hexadecimal are removed.\n"
                     "  -h        display this help and exit");
                 exit(EXIT_SUCCESS);
                 break;
@@ -54,18 +73,42 @@ int main(int argc, char *argv[])
             DIE("invalid mapping: second character is not the separator");
     }
 
-    while ((nread = read(0, buf, bufsize)) > 0) {
-        for (n = 0; n < nread; n++) {
-            c = buf[n];
-            for (i = 0; i < argc; i++) {
-                p = argv[i][0];
-                if (c == p) {
-                    fputs(argv[i] + 2, stdout);
-                    goto nextchar;
+    if (!optbin) {
+        while ((nread = read(0, buf, bufsize)) > 0) {
+            for (n = 0; n < nread; n++) {
+                c = buf[n];
+                for (i = 0; i < argc; i++) {
+                    p = argv[i][0];
+                    if (c == p) {
+                        fputs(argv[i] + 2, stdout);
+                        goto nextchar;
+                    }
+                }
+                putchar(c);
+nextchar:;
+            }
+        }
+    } else {
+        bin = 0;
+        binoffset = 0;
+        while ((nread = read(0, buf, bufsize)) > 0) {
+            for (n = 0; n < nread; n++) {
+                c = buf[n];
+                for (i = 0; i < binsize; i++) {
+                    p = binmappings[i].key;
+                    if (c == p) {
+                        bin |= binmappings[i].val << binoffset;
+                        if (binoffset == 4) {
+                            putchar(bin);
+                            binoffset = 0;
+                            bin = 0;
+                        } else {
+                            binoffset = 4;
+                        }
+                        break;
+                    }
                 }
             }
-            putchar(c);
-nextchar:;
         }
     }
 
