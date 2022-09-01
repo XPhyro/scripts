96c64e7a5df51265698dc0124d1aed37239f2b27
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2022-09-01 05:54:51 +0300

    Refactor shvector

--- src/cpp/util/sh/shvector.cpp
+++ src/cpp/util/sh/shvector.cpp
@@ -31,8 +31,11 @@ typedef enum {
     CACHE_PERSISTENT,
 } cache_t;
 
-std::pair<size_t, std::string> readcache(std::string flname);
 HEDLEY_NO_RETURN void die(const std::string err);
+std::pair<size_t, std::string> readcache(std::string flname);
+void vecout(std::string cachefl);
+void vecnew(std::string cachefl);
+void vecsize(std::string cachefl);
 
 const std::unordered_map<std::string, cache_t> caches = {
     { "t", CACHE_TEMPORARY },    { "tmp", CACHE_TEMPORARY },
@@ -44,7 +47,7 @@ const char* execname;
 
 int main(int argc, char* argv[])
 {
-    std::string proghash, arrname;
+    std::string proghash, vecname;
     const std::vector<std::string> v;
     cache_t cache = CACHE_TEMPORARY;
     int i;
@@ -71,21 +74,21 @@ int main(int argc, char* argv[])
                        "Handle arrays in a strictly POSIX shell.\n"
                        "\n"
                        "SYNTAX may be one of:\n"
-                       "1. [ARR_NAME]\n"
+                       "1. [VEC_NAME]\n"
                        "   1. Get array.\n"
                        "   2. Array must have been initialised.\n"
-                       "2. [ARR_NAME] [INDEX]\n"
+                       "2. [VEC_NAME] [INDEX]\n"
                        "   1. Get value at INDEX.\n"
                        "   2. INDEX must be in range [0, size).\n"
                        "   3. Array must have been initialised.\n"
-                       "3. [ARR_NAME] = [OTHER_ARR_NAME]\n"
-                       "   1. Copy OTHER_ARR_NAME to ARR_NAME.\n"
-                       "   2. If OTHER_ARR_NAME is NULL or nullptr, uninitialise array.\n"
-                       "4. [ARR_NAME] [INDEX] = [VALUE]\n"
+                       "3. [VEC_NAME] = [OTHER_VEC_NAME]\n"
+                       "   1. Copy OTHER_VEC_NAME to VEC_NAME.\n"
+                       "   2. If OTHER_VEC_NAME is NULL or nullptr, uninitialise array.\n"
+                       "4. [VEC_NAME] [INDEX] = [VALUE]\n"
                        "   1. Set value at INDEX to VALUE.\n"
                        "   2. INDEX must be in range [0, size).\n"
                        "   3. Array must have been initialised.\n"
-                       "5. [ARR_NAME] [FUNC]\n"
+                       "5. [VEC_NAME] [FUNC]\n"
                        "   1. new\n"
                        "      1. Initialise array.\n"
                        "      2. If already initialised, array is reinitialised.\n"
@@ -113,7 +116,7 @@ int main(int argc, char* argv[])
         die("invalid syntax");
 
     proghash = argv[0];
-    arrname = argv[1];
+    vecname = argv[1];
     argv += 2;
     argc -= 2;
 
@@ -142,58 +145,35 @@ int main(int argc, char* argv[])
         std::ostringstream ss;
         ss << prefix << '/' << givenexecname << '/' << proghash;
         cachedir = ss.str();
-        ss << '/' << arrname;
+        ss << '/' << vecname;
         cachefl = ss.str();
     }
 
     rmkdirconst(cachedir.c_str(), 0755);
 
     switch (argc) {
-        case 0: {
-            auto cache = readcache(cachefl);
-            auto size = cache.first;
-            auto buf = cache.second;
-
-            for (auto&& view :
-                 buf | std::views::split('\0') | std::views::transform([](auto&& r) -> std::string {
-                     return { &*r.begin(),
-                              static_cast<std::string::size_type>(std::ranges::distance(r)) };
-                 }) | std::views::take(size)) {
-                std::cout << view << '\0';
-            }
-        } break;
-        case 1: {
-            if (streq(argv[0], "new")) {
-                std::ofstream fl(cachefl, std::ios::out | std::ios::binary);
-
-                if (!fl)
-                    die("could not open cache file");
-
-                size_t size = 2;
-                fl.write(reinterpret_cast<char*>(&size), sizeof(size));
-                fl.write("\0", 1);
-
-                fl.close();
-            } else if (streq(argv[0], "size")) {
-                auto cache = readcache(cachefl);
-                auto size = cache.first;
-                std::cout << size;
-            } else {
-                // TODO: if parseable to int, index
-            }
-        } break;
-        case 2: {
-            if (streq(argv[0], "=")) {
-                // TODO: copy
-            } else if (streq(argv[0], "push_back")) {
-                // TODO: append
-            }
-        } break;
-        case 3: {
+        case 0:
+            vecout(cachefl);
+            break;
+        case 1:
+            if (streq(argv[0], "new"))
+                vecnew(cachefl);
+            else if (streq(argv[0], "size"))
+                vecsize(cachefl);
+            else
+                ; // TODO: if parseable to int, index
+            break;
+        case 2:
+            if (streq(argv[0], "="))
+                ; // TODO: copy
+            else if (streq(argv[0], "push_back"))
+                ; // TODO: append
+            break;
+        case 3:
             // TODO: if argv[0] is parseable to int and argv[1] is =, set index
-        } break;
-        default: {
-        } break;
+            break;
+        default:
+            break;
     }
 
     return EXIT_SUCCESS;
@@ -210,7 +190,7 @@ std::pair<size_t, std::string> readcache(std::string flname)
     std::ifstream fl(flname, std::ios::in | std::ios::binary);
 
     if (!fl)
-        die("could not open cache file");
+        die("could not open cache file for reading");
 
     size_t size;
     fl.read(reinterpret_cast<char*>(&size), sizeof(size));
@@ -225,4 +205,39 @@ std::pair<size_t, std::string> readcache(std::string flname)
     }
 
     return { size, buf };
+}
+
+void vecout(std::string cachefl)
+{
+    auto cache = readcache(cachefl);
+    auto size = cache.first;
+    auto buf = cache.second;
+
+    for (auto&& view :
+         buf | std::views::split('\0') | std::views::transform([](auto&& r) -> std::string {
+             return { &*r.begin(), static_cast<std::string::size_type>(std::ranges::distance(r)) };
+         }) | std::views::take(size)) {
+        std::cout << view << '\0';
+    }
+}
+
+void vecnew(std::string cachefl)
+{
+    std::ofstream fl(cachefl, std::ios::out | std::ios::binary);
+
+    if (!fl)
+        die("could not open cache file for writing");
+
+    size_t size = 2;
+    fl.write(reinterpret_cast<char*>(&size), sizeof(size));
+    fl.write("\0", 1);
+
+    fl.close();
+}
+
+void vecsize(std::string cachefl)
+{
+    auto cache = readcache(cachefl);
+    auto size = cache.first;
+    std::cout << size;
 }
