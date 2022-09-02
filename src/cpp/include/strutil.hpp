69cc576b208f7bb50a3ad173ae27108152866520
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2022-09-02 04:19:08 +0300

    Add new functions and mark applicable as constexpr in strutil C++ header

--- src/cpp/include/strutil.hpp
+++ src/cpp/include/strutil.hpp
@@ -3,7 +3,10 @@
 
 #include <algorithm>
 #include <memory>
+#include <ranges>
+#include <sstream>
 #include <string>
+#include <vector>
 
 namespace strutil
 {
@@ -16,12 +19,30 @@ std::unique_ptr<std::string> getlower(const std::string str)
     return std::make_unique<std::string>(*dup);
 }
 
-std::string makelower(std::string str)
+std::string constexpr makelower(std::string str)
 {
     std::transform(str.begin(), str.end(), str.begin(), [](unsigned char c) {
         return std::tolower(c);
     });
     return str;
+}
+
+auto constexpr splitview(char delim)
+{
+    return std::views::split(delim) | std::views::transform([](const auto&& r) -> std::string {
+               return { &*r.begin(),
+                        static_cast<std::string::size_type>(std::ranges::distance(r)) };
+           });
+}
+
+std::vector<std::string> constexpr split(const std::string& str, char delim)
+{
+    std::vector<std::string> tokens;
+
+    for (auto&& token : str | splitview(delim))
+        tokens.push_back(token);
+
+    return std::move(tokens);
 }
 } // namespace strutil
 
