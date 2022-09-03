8a9642006c520c16afb656604f5b2418ca2ae590
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2022-09-04 02:14:22 +0300

    Create nullable C++ header

---
+++ src/cpp/include/nullable.hpp
@@ -0,0 +1,62 @@
+#ifndef HEADER_SCRIPTS_CXX_NULLABLE
+#define HEADER_SCRIPTS_CXX_NULLABLE
+
+#include <ostream>
+
+template <typename T>
+class nullable {
+public:
+    nullable(T t)
+    {
+        value = t;
+    }
+
+    nullable(T& t)
+        : value(t)
+    {
+    }
+
+    nullable(T&& t)
+        : value(t)
+    {
+    }
+
+    nullable operator=(const T& t)
+    {
+        is_null = t == T{};
+        value = t;
+        return *this;
+    }
+
+    bool operator==(const T& t)
+    {
+        return value == t;
+    }
+
+    bool operator!=(const T& t)
+    {
+        return value != t;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const nullable& n)
+    {
+        os << n.value;
+        return os;
+    }
+
+    operator const T&()
+    {
+        return std::move(value);
+    }
+
+    operator bool()
+    {
+        return !is_null;
+    }
+
+private:
+    bool is_null = true;
+    T value;
+};
+
+#endif /* ifndef HEADER_SCRIPTS_CXX_NULLABLE */
