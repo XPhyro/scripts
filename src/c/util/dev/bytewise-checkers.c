e2bd0fe5dc1fd9f05d4a4b5e7305bc3f3114c1c1
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2022-09-20 06:37:41 +0300

    Create c/util/dev/bytewise-checkers.c

---
+++ src/c/util/dev/bytewise-checkers.c
@@ -0,0 +1,13 @@
+#include <limits.h>
+#include <stddef.h>
+#include <sys/param.h>
+#include <unistd.h>
+
+#include <strutil.h>
+
+int main(int argc, char *argv[])
+{
+    unsigned char cur = 1, data[PIPE_BUF];
+    for (size_t i = 0; i < PIPE_BUF; data[i++] = (cur ^= 1)) {}
+    for (;; write(STDOUT_FILENO, data, PIPE_BUF)) {}
+}
