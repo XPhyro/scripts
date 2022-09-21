704a2758c9e5e42657beada4287ff3eb6b94944d
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2022-09-21 03:44:50 +0300

    Create tictoc C++ header

---
+++ src/cpp/include/tictoc.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <chrono>
+#include <string>
+#include <unordered_map>
+
+namespace xph
+{
+static std::unordered_map<std::string, std::chrono::steady_clock::time_point> times{};
+
+void tic(const std::string& hash)
+{
+    times.emplace(hash, std::chrono::steady_clock::now());
+}
+
+[[nodiscard]] auto toc(const std::string& hash)
+{
+    return std::chrono::steady_clock::now() - times[hash];
+}
+
+auto toctic(const std::string& hash)
+{
+    auto time_point = std::chrono::steady_clock::now();
+    auto ret = time_point - times[hash];
+    times[hash] = time_point;
+    return ret;
+}
+} // namespace xph
