c310300db924e0e3ee9fc2a9b93d3a4ca68195fa
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2022-10-05 10:47:14 +0300

    Add new functions in utility.sh

--- src/sh/include/utility.sh
+++ src/sh/include/utility.sh
@@ -3,6 +3,25 @@
 
     execname="${0##*/}"
 
+    std_logferr() {
+        fmt="$1"
+        shift
+        printf "%s: $fmt" "$execname" "$@" >&2
+    }
+
+    std_logerr() {
+        for i; do
+            printf "%s: %s\n" "$execname" "$i" >&2
+        done
+    }
+
+    std_logferrq() {
+        fmt="$1"
+        shift
+        printf "%s: $fmt" "$execname" "$@" >&2
+        exit "${ec:-1}"
+    }
+
     std_logerrq() {
         for i; do
             printf "%s: %s\n" "$execname" "$i" >&2
