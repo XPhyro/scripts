a6d0f0555031c2c446456bb5481c378240595ea6
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2022-10-05 10:38:48 +0300

    Add header guard in utility.sh

--- src/sh/include/utility.sh
+++ src/sh/include/utility.sh
@@ -1,8 +1,12 @@
-execname="${0##*/}"
+[ -z "$HEADER_SCRIPTS_UTILITY_" ] && {
+    HEADER_SCRIPTS_UTILITY_=1
 
-std_logerrq() {
-    for i; do
-        printf "%s: %s\n" "$execname" "$i" >&2
-    done
-    exit "${ec:-1}"
+    execname="${0##*/}"
+
+    std_logerrq() {
+        for i; do
+            printf "%s: %s\n" "$execname" "$i" >&2
+        done
+        exit "${ec:-1}"
+    }
 }
