de4f0c2ebd1d86823875b53cd626cd357ba0da5b
Berke Kocaoğlu <kberke@metu.edu.tr>
2022-12-02 20:35:19 +0300

    lexical_cast.hpp: add specialisations for primitive numeric values

--- src/cpp/include/lexical_cast.hpp
+++ src/cpp/include/lexical_cast.hpp
@@ -24,11 +24,56 @@ namespace xph {
         return b;
     }
 
-    inline std::string lexical_cast(const bool& b)
+    inline std::string lexical_cast(const bool& arg)
     {
         std::ostringstream ss;
-        ss << std::boolalpha << b;
+        ss << std::boolalpha << arg;
         return ss.str();
+    }
+
+    inline std::string lexical_cast(const int& arg)
+    {
+        return std::to_string(arg);
+    }
+
+    inline std::string lexical_cast(const long& arg)
+    {
+        return std::to_string(arg);
+    }
+
+    inline std::string lexical_cast(const long long& arg)
+    {
+        return std::to_string(arg);
+    }
+
+    inline std::string lexical_cast(const unsigned& arg)
+    {
+        return std::to_string(arg);
+    }
+
+    inline std::string lexical_cast(const unsigned long& arg)
+    {
+        return std::to_string(arg);
+    }
+
+    inline std::string lexical_cast(const unsigned long long& arg)
+    {
+        return std::to_string(arg);
+    }
+
+    inline std::string lexical_cast(const float& arg)
+    {
+        return std::to_string(arg);
+    }
+
+    inline std::string lexical_cast(const double& arg)
+    {
+        return std::to_string(arg);
+    }
+
+    inline std::string lexical_cast(const long double& arg)
+    {
+        return std::to_string(arg);
     }
 } // namespace xph
 
