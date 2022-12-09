71fd37ad1b3a40d3e97945fdae63c0e11da515c6
Berke Kocaoğlu <kberke@metu.edu.tr>
2022-12-09 04:03:26 +0300

    ioutil.h: refactor

--- src/c/include/ioutil.h
+++ src/c/include/ioutil.h
@@ -2,13 +2,13 @@
 #define HEADER_SCRIPTS_C_IOUTIL_
 
 #ifdef _POSIX_C_SOURCE
-#define BAK_POSIX_C_SOURCE _POSIX_C_SOURCE
+#define BAK_POSIX_C_SOURCE_ _POSIX_C_SOURCE
 #undef _POSIX_C_SOURCE
 #define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #undef _POSIX_C_SOURCE
-#define _POSIX_C_SOURCE BAK_POSIX_C_SOURCE
-#undef BAK_POSIX_C_SOURCE
+#define _POSIX_C_SOURCE BAK_POSIX_C_SOURCE_
+#undef BAK_POSIX_C_SOURCE_
 #else /* ifdef _POSIX_C_SOURCE */
 #define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
@@ -34,14 +34,11 @@ char *fgetstr(FILE *stream, int delim)
 char *getstr(int argc, char *argv[], int delim)
 {
     static int i = 0;
-    static char *line = NULL;
 
     if (argc)
         return i < argc ? argv[i++] : NULL;
-    else if ((line = fgetstr(stdin, delim)))
-        return line;
 
-    return NULL;
+    return fgetstr(stdin, delim);
 }
 
 #endif /* ifndef HEADER_SCRIPTS_C_IOUTIL_ */
