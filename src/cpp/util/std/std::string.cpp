d66b7aecdad1ddc419e88bee7c7445f14dee5dc7
Berke Kocaoğlu <kberke@metu.edu.tr>
2022-12-19 10:54:33 +0300

    std::string: add align subcommand

--- src/cpp/util/std/std::string.cpp
+++ src/cpp/util/std/std::string.cpp
@@ -2,7 +2,9 @@
 #include <ranges>
 #include <sstream>
 #include <string>
+#include <string_view>
 #include <unordered_map>
+#include <vector>
 
 #include <unistd.h>
 
@@ -13,9 +15,12 @@
 
 DEFINE_EXEC_INFO();
 
+std::string get_input(int& argc, char**& argv);
+void align(int& argc, char**& argv);
 void replace_all(int& argc, char**& argv);
 
 std::unordered_map<std::string, std::function<void(int&, char**&)>> functions = {
+    { "align", align },
     { "replace_all", replace_all },
 };
 
@@ -42,6 +47,107 @@ int main(int argc, char* argv[])
     return EXIT_SUCCESS;
 }
 
+std::string get_input(int& argc, char**& argv)
+{
+    std::stringstream ss;
+
+    // TODO: don't load whole strings into memory, process by chunks.
+    if (!argc) {
+        ss << std::cin.rdbuf();
+    } else {
+        for (const auto& arg : std::views::counted(argv, argc - 1)) {
+            ss << arg;
+            ss << '\n';
+        }
+        ss << argv[argc - 1];
+    }
+
+    return ss.str();
+}
+
+void align(int& argc, char**& argv)
+{
+    if (argc < 1)
+        xph::die("insufficient number of operands given");
+
+    if (!*argv[0])
+        xph::die("alignment string cannot be empty");
+
+    std::string align = argv[0];
+    --argc;
+    ++argv;
+
+    std::vector<std::size_t> max_positions;
+    std::vector<std::vector<std::size_t>> positions;
+    std::vector<std::string> lines;
+
+    {
+        std::size_t i = 0;
+        auto input = get_input(argc, argv);
+        for (const auto& line : input | xph::str::splitview('\n')) {
+            positions.push_back({});
+            lines.push_back({ line });
+
+            auto pos = line.find(align, 0);
+            std::size_t j = 0;
+
+            while (pos != std::string::npos) {
+                positions[i].push_back(pos);
+
+                if (max_positions.size() < j + 1)
+                    max_positions.push_back(pos);
+                else if (pos > max_positions[j])
+                    max_positions[j] = pos;
+
+                pos = line.find(align, pos + 1);
+
+                ++j;
+            }
+
+            ++i;
+        }
+    }
+
+    for (auto&& it = max_positions.begin() + 1; it != max_positions.end(); ++it) {
+        if (*it < *(it - 1))
+            *it = *(it - 1) + 1;
+    }
+
+    for (std::size_t l = 0; l < lines.size(); ++l) {
+        const auto& line = lines[l];
+
+        if (auto& line_positions = positions[l]; line_positions.empty()) {
+            std::cout << line << '\n';
+        } else {
+            line_positions.reserve(line_positions.size() + 1); // force small allocation
+            line_positions.push_back(line.size());
+
+            std::size_t current_pos = 0;
+
+            if (auto first_pos = line_positions.front(); first_pos != 0) {
+                std::cout << std::string_view(line.begin(), line.begin() + first_pos);
+                current_pos += first_pos;
+            }
+
+            for (std::size_t lp = 0; lp < line_positions.size() - 1; ++lp) {
+                auto pos = line_positions[lp];
+                auto next_pos = line_positions[lp + 1];
+                auto max_pos = max_positions[lp];
+
+                if (max_pos > current_pos) {
+                    for ([[maybe_unused]] const auto&& _ :
+                         std::views::iota(0u, max_pos - current_pos))
+                        std::cout << ' ';
+                }
+                std::cout << std::string_view(line.begin() + pos, line.begin() + next_pos);
+                current_pos += max_positions[lp] - current_pos - 1 + next_pos - pos;
+            }
+
+            std::cout << '\n';
+        }
+    }
+}
+
 void replace_all(int& argc, char**& argv)
 {
     if (argc < 2)
@@ -54,22 +160,8 @@ void replace_all(int& argc, char**& argv)
     argc -= 2;
     argv += 2;
 
-    std::string str; // TODO: optimise
+    auto input = get_input(argc, argv);
 
-    if (argc > 0) {
-        std::stringstream ss;
-        for (const auto& arg : std::views::counted(argv, argc - 1)) {
-            ss << arg;
-            ss << ' ';
-        }
-        ss << argv[argc - 1];
-        str = ss.str();
-    } else {
-        std::stringstream ss;
-        ss << std::cin.rdbuf();
-        str = ss.str();
-    }
-
-    xph::str::replaceall(str, from, to);
-    std::cout << str;
+    xph::str::replaceall(input, from, to);
+    std::cout << input;
 }
