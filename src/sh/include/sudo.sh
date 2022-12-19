09411d423baa11a862def34a3685d3dc24ed808e
Berke Kocaoğlu <kberke@metu.edu.tr>
2022-12-19 16:29:34 +0300

    sudo.sh: prioritise doas over sudo

--- src/sh/include/sudo.sh
+++ src/sh/include/sudo.sh
@@ -5,10 +5,10 @@
 
     if [ "$(id -u)" == 0 ]; then
         unset __sudo_cmd
-    elif std_has_command sudo; then
-        __sudo_cmd="sudo"
     elif std_has_command doas; then
         __sudo_cmd="doas"
+    elif std_has_command sudo; then
+        __sudo_cmd="sudo"
     else
         unset __sudo_cmd
     fi
