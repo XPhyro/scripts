9dad78de857c0b9f5fa99f5dded67542fdffc045
Berke Kocaoğlu <kberke@metu.edu.tr>
2023-01-14 02:28:09 +0300

    prefix.sh: support asserting and checking whether prefix can be queried

--- src/sh/include/prefix.sh
+++ src/sh/include/prefix.sh
@@ -3,9 +3,21 @@
 [ -z "${HEADER_SCRIPTS_PREFIX_+x}" ] && {
     HEADER_SCRIPTS_PREFIX_=
 
-    std_prefix="${0%%/bin/"${0##*/}"}"
+    . execinfo.sh
+    . io.sh
+
+    case "$std_execpath" in
+        /*) std_can_query_prefix=1;;
+        *) std_can_query_prefix=0;;
+    esac
+
+    std_prefix="${std_execpath%%/bin/"${std_execpath##*/}"}"
     std_binprefix="$std_prefix/bin"
     std_includeprefix="$std_prefix/include"
     std_manprefix="$std_prefix/share/man"
     std_dataprefix="$std_prefix/share/scripts"
+
+    std_assert_can_query_prefix() {
+        [ "$std_can_query_prefix" -eq 0 ] && std_logferrq 'cannot query prefix. $0 is %s.\n' "$0"
+    }
 }
