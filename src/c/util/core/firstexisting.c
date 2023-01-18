dcc577705f84b31301bdb252dfd0ad2f5b089784
Berke Kocaoğlu <kberke@metu.edu.tr>
2023-01-18 12:59:03 +0300

    Create c/util/core/firstexisting.c

---
+++ src/c/util/core/firstexisting.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    int i;
+    const char *path;
+
+    for (i = 1; i < argc; i++) {
+        path = argv[i];
+        if (!access(path, F_OK)) {
+            fputs(path, stdout);
+            return 0;
+        }
+    }
+
+    return 1;
+}
