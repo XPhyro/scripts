XPhyro/scripts

commit 70d0d62430ca308d17dc379308518511cd77c43c

*: fix typos

src/rs/termctl/src/main.rs
@@ -91,7 +91,7 @@ fn clear(subcommand: String, _args: Vec<String>) -> Result<(), ExitFailure> {
         "before_cursor" => print!("{}", termion::clear::BeforeCursor),
         "current_line" => print!("{}", termion::clear::CurrentLine),
         "until_newline" => print!("{}", termion::clear::UntilNewline),
-        _ => return Err(io::Error::new(io::ErrorKind::InvalidInput, "Unkown clear mode").into()),
+        _ => return Err(io::Error::new(io::ErrorKind::InvalidInput, "Unknown clear mode").ilto()),
     };
     return Ok(());
 }

src/cpp/util/std/std::string.cpp
@@ -42,7 +42,7 @@ int main(int argc, char* argv[])
         }
     }
 
-    xph::die("unkown function: ", requested_function_name);
+    xph::die("unknown function: ", requested_function_name);
 
     return EXIT_SUCCESS;
 }

commit 13766cb8597706aa3b05496b72b670c917dda5b1

termctl: fix compilation error introduced in 70d0d62430ca

src/rs/termctl/src/main.rs
@@ -91,7 +91,7 @@ fn clear(subcommand: String, _args: Vec<String>) -> Result<(), ExitFailure> {
         "before_cursor" => print!("{}", termion::clear::BeforeCursor),
         "current_line" => print!("{}", termion::clear::CurrentLine),
         "until_newline" => print!("{}", termion::clear::UntilNewline),
-        _ => return Err(io::Error::new(io::ErrorKind::InvalidInput, "Unknown clear mode").ilto()),
+        _ => return Err(io::Error::new(io::ErrorKind::InvalidInput, "Unknown clear mode").into()),
     };
     return Ok(());
 }