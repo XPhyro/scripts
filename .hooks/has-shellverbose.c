5b85c4949e2ca18c767abaabe2556ed63d13e9df
Berke Kocaoğlu <kberke@metu.edu.tr>
2023-06-07 11:45:06 +0300

    Create .hooks/has-shellverbose.c

---
+++ .hooks/has-shellverbose.c
@@ -0,0 +1,77 @@
+#!/usr/bin/env gcc-otg
+
+// @CXXFLAGS -std=c99 -Wall -Werror -pedantic
+// @LDFLAGS
+
+#define _POSIX_C_SOURCE 200809L
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define EXECNAME "has-shellverbose"
+
+int main(int argc, char *argv[])
+{
+    argc--;
+    argv++;
+
+    while (argc--) {
+        const char *filename = *argv;
+        FILE *stream;
+        if (!(stream = fopen(filename, "r"))) {
+            if (errno == ENOENT)
+                fprintf(stderr, EXECNAME ": %s: No such file or directory\n", filename);
+            else {
+                fprintf(stderr, EXECNAME ": %s: Could not open file: ", filename);
+                perror("fopen");
+            }
+
+            return EXIT_FAILURE;
+        }
+
+        char *line = NULL;
+        size_t line_size = 0;
+        ssize_t line_len;
+        while ((line_len = getline(&line, &line_size, stream)) != -1) {
+            if (line[line_len - 1] == '\n') {
+                line[line_len - 1] = '\0';
+                line_len--;
+            }
+            for (size_t i = 0; i < line_len; i++) {
+                while (i < line_len) {
+                    if (line[i] == ' ')
+                        i++;
+                    else
+                        break;
+                }
+                if (line[i] == '#') {
+                    goto next_line;
+                }
+                if (line[i] == '.') {
+                    for (i++; i < line_len;) {
+                        if (line[i] == ' ')
+                            i++;
+                        else
+                            break;
+                    }
+                    if (strcmp(line + i, "shellverbose.sh"))
+                        printf("File %s does not have shellverbose.sh at the top\n", filename);
+                } else {
+                    printf("File %s does not have shellverbose.sh at the top\n", filename);
+                }
+                goto next_file;
+            }
+            
+next_line:;
+        }
+next_file:;
+
+        fclose(stream);
+
+        argv++;
+    }
+
+    return EXIT_SUCCESS;
+}
