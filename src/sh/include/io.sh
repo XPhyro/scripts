5ba3810c2046dd178b09ad83581f8cb5c865962b
Berke Kocaoğlu <kberke@metu.edu.tr>
2023-06-07 11:42:02 +0300

    io.sh: add `std_backup_stdin`, `std_is_stdin_empty` and `std_is_stdin_tty`

--- src/sh/include/io.sh
+++ src/sh/include/io.sh
@@ -3,6 +3,36 @@
 
     . execinfo.sh
 
+    unset std_stdin_backup
+    __std_is_stdin_backed_up=0
+
+    __std_backup_stdin_impl() {
+        [ "$__std_is_stdin_backed_up" -ne 0 ] && return 0
+        std_stdin_backup="$(mktemp -t std_backup_stdin.XXXXXXXXXX)"
+        # not sure if `cp` introduces security issues, so use `cat`
+        cat - > "$std_stdin_backup"
+    }
+
+    std_backup_stdin() {
+        if [ "$#" -eq 0 ]; then
+            __std_backup_stdin_impl
+        else
+            __std_backup_stdin_impl
+            for __i; do
+                # not sure if `cp` introduces security issues, so use `cat`
+                cat "$std_stdin_backup" > "$__i"
+            done
+        fi
+    }
+
+    std_is_stdin_empty() {
+        [ "$(fionread)" -eq 0 ] # TODO: find a shell-only solution for this
+    }
+
+    std_is_stdin_tty() {
+        [ -t 0 ]
+    }
+
     std_logf() {
         __fmt="$1"
         shift
