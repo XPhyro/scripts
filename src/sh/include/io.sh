08e6c0dd17aa5e9cba8e24bbef2fb8df8db6c810
Berke Kocaoğlu <kberke@metu.edu.tr>
2023-06-13 02:38:15 +0300

    io.sh: don't hijack built-in printf

--- src/sh/include/io.sh
+++ src/sh/include/io.sh
@@ -4,8 +4,8 @@
     . execinfo.sh
     . meta.sh
 
-    printf() {
-        ! std_is_set IN_HEADER_SPACE_ || ! std_is_set SUPPRESS_PRINTF_ && command printf "$@"
+    __std_printf() {
+        std_is_set SUPPRESS_PRINTF_ || printf "$@"
     }
 
     unset std_stdin_backup
@@ -42,96 +42,96 @@
         __fmt="$1"
         shift
         # shellcheck disable=SC2154
-        printf "%s: $__fmt" "$execname" "$@"
+        __std_printf "%s: $__fmt" "$execname" "$@"
     }
 
     std_log() {
         for __i; do
-            printf "%s: %s\n" "$execname" "$__i"
+            __std_printf "%s: %s\n" "$execname" "$__i"
         done
     }
 
     std_lograw() {
         for __i; do
-            printf "%s\n" "$__i"
+            __std_printf "%s\n" "$__i"
         done
     }
 
     std_logfraw() {
-        printf "$@"
+        __std_printf "$@"
     }
 
     std_logfq() {
         __fmt="$1"
         shift
-        printf "%s: $__fmt" "$execname" "$@"
+        __std_printf "%s: $__fmt" "$execname" "$@"
         exit "${ec:-1}"
     }
 
     std_logq() {
         for __i; do
-            printf "%s: %s\n" "$execname" "$__i"
+            __std_printf "%s: %s\n" "$execname" "$__i"
         done
         exit "${ec:-1}"
     }
 
     std_lograwq() {
         for __i; do
-            printf "%s\n" "$__i"
+            __std_printf "%s\n" "$__i"
         done
         exit "${ec:-1}"
     }
 
     std_logfrawq() {
-        printf "$@"
+        __std_printf "$@"
         exit "${ec:-1}"
     }
 
     std_logferr() {
         __fmt="$1"
         shift
-        printf "%s: $__fmt" "$execname" "$@" >&2
+        __std_printf "%s: $__fmt" "$execname" "$@" >&2
     }
 
     std_logerr() {
         for __i; do
-            printf "%s: %s\n" "$execname" "$__i" >&2
+            __std_printf "%s: %s\n" "$execname" "$__i" >&2
         done
     }
 
     std_logerrraw() {
         for __i; do
-            printf "%s\n" "$__i" >&2
+            __std_printf "%s\n" "$__i" >&2
         done
     }
 
     std_logferrraw() {
-        printf "$@" >&2
+        __std_printf "$@" >&2
     }
 
     std_logferrq() {
         __fmt="$1"
         shift
-        printf "%s: $__fmt" "$execname" "$@" >&2
+        __std_printf "%s: $__fmt" "$execname" "$@" >&2
         exit "${ec:-1}"
     }
 
     std_logerrq() {
         for __i; do
-            printf "%s: %s\n" "$execname" "$__i" >&2
+            __std_printf "%s: %s\n" "$execname" "$__i" >&2
         done
         exit "${ec:-1}"
     }
 
     std_logerrrawq() {
         for __i; do
-            printf "%s\n" "$__i" >&2
+            __std_printf "%s\n" "$__i" >&2
         done
         exit "${ec:-1}"
     }
 
     std_logferrrawq() {
-        printf "$@" >&2
+        __std_printf "$@" >&2
         exit "${ec:-1}"
     }
 }
