4494e19ce88bd2b8227ee16f5c8f5eb0bc0d24e1
Berke Kocaoğlu <kberke@metu.edu.tr>
2023-06-13 02:58:26 +0300

    paramutil.h: add `ARRLEN()`

--- src/c/include/paramutil.h
+++ src/c/include/paramutil.h
@@ -4,6 +4,8 @@
 #define STRINGIFY_IMPL_(VAL) #VAL
 #define STRINGIFY(VAL) STRINGIFY_IMPL_(VAL)
 
+#define ARRLEN(ARR) (sizeof(ARR) / sizeof((ARR)[0]))
+
 extern int _iso_c_forbids_an_empty_translation_unit;
 
 #endif /* ifndef HEADER_SCRIPTS_C_PARAMUTIL_ */
