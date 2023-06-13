c450975b143b22b1a778f4008572e0ba33769bbd
Berke Kocaoğlu <kberke@metu.edu.tr>
2023-06-13 21:48:34 +0300

    auto_timer.hpp: offload unit conversion to unitutil.hpp

--- src/cpp/include/auto_timer.hpp
+++ src/cpp/include/auto_timer.hpp
@@ -2,6 +2,7 @@
 #define HEADER_SCRIPTS_CXX_AUTO_TIMER_
 
 #include <logger.hpp>
+#include <unitutil.hpp>
 
 namespace xph {
     class auto_timer {
@@ -35,46 +36,12 @@ namespace xph {
 
             dumped = true;
 
-            long double scaled_duration;
-            const char* unit;
-
-            if (auto duration = xph::toc(km_identifier).count(); !duration) {
+            auto duration = xph::toc(km_identifier).count();
+            if (!duration)
                 return;
-            } else {
-                if (duration < 1'000) {
-                    scaled_duration = duration;
-                    unit = "ns";
-                } else if (duration < 1'000'000) {
-                    scaled_duration = duration / 1'000.0l;
-                    unit = "us";
-                } else if (duration < 1'000'000'000) {
-                    scaled_duration = duration / 1'000'000.0l;
-                    unit = "ms";
-                } else if (duration < 60'000'000'000) {
-                    scaled_duration = duration / 1'000'000'000.0l;
-                    unit = "s";
-                } else if (duration < 3'600'000'000'000) {
-                    scaled_duration = duration / 60'000'000'000.0l;
-                    unit = "m";
-                } else if (duration < 86'400'000'000'000) {
-                    scaled_duration = duration / 3'600'000'000'000.0l;
-                    unit = "h";
-                } else if (duration < 604'800'000'000'000) {
-                    scaled_duration = duration / 86'400'000'000'000.0l;
-                    unit = "d";
-                } else if (duration < 2'629'746'000'000'000) {
-                    scaled_duration = duration / 604'800'000'000'000.0l;
-                    unit = "w";
-                } else if (duration < 31'556'952'000'000'000) {
-                    scaled_duration = duration / 2'629'746'000'000'000.0l;
-                    unit = "m";
-                } else {
-                    scaled_duration = duration / 31'556'952'000'000'000.0l;
-                    unit = "y";
-                }
-            }
 
-            m_logger->log(km_log_level, km_identifier, " took ", scaled_duration, unit);
+            m_logger->log(
+                km_log_level, km_identifier, " took ", xph::unit::tick_to_human(duration));
         }
     };
 } // namespace xph
