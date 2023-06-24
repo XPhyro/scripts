c590e3900a03fbfe3173ba7e606e81aa60772a8f
Berke Kocaoğlu <kberke@metu.edu.tr>
2023-06-24 16:47:37 +0300

    bytewise-checkers: refactor

--- src/c/util/dev/bytewise-checkers.c
+++ src/c/util/dev/bytewise-checkers.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char *argv[])
 {
-    unsigned char cur = 1, data[PIPE_BUF];
-    for (size_t i = 0; i < PIPE_BUF; data[i++] = (cur ^= 1)) {}
+    unsigned char data[PIPE_BUF];
+    for (size_t i = 0; i < PIPE_BUF; data[i] = i & 1, i++) {}
     for (;; write(STDOUT_FILENO, data, PIPE_BUF)) {}
 }
