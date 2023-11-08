d55579c2872d047de834382eb18cb247cd258db9
Berke Kocaoğlu <kberke@metu.edu.tr>
2023-11-08 21:53:09 +0300

    shellverbose.sh: use value as level of verbosity

--- src/sh/include/shellverbose.sh
+++ src/sh/include/shellverbose.sh
@@ -1,5 +1,10 @@
 [ -z "${HEADER_SCRIPTS_SH_EVALVERBOSE_+x}" ] && {
     HEADER_SCRIPTS_SH_EVALVERBOSE_=
 
-    [ -n "$SHELL_VERBOSE" ] && [ "$SHELL_VERBOSE" -gt 0 ] 2> /dev/null && set -x || :
+    [ -n "$SHELL_VERBOSE" ] && [ "$SHELL_VERBOSE" -gt 0 ] 2> /dev/null && {
+        set -x
+        SHELL_VERBOSE="$((SHELL_VERBOSE - 1))"
+        export SHELL_VERBOSE
+    }
+    : # zero exit code
 }
