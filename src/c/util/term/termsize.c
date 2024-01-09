c698a6e1ccea79d98a795165aed66f24ce032b06
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2024-01-09 23:28:59 +0300

    termsize: output to files if given

--- src/c/util/term/termsize.c
+++ src/c/util/term/termsize.c
@@ -1,12 +1,30 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
     struct winsize w;
+    int ret = EXIT_SUCCESS;
+    FILE *fl;
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-    printf("%d %d\n", w.ws_xpixel, w.ws_ypixel);
-    return EXIT_SUCCESS;
+
+    if (argc <= 1) {
+        printf("%d %d\n", w.ws_xpixel, w.ws_ypixel);
+    } else {
+        for (int i = 1; i < argc; i++) {
+            if (!(fl = fopen(argv[i], "w"))) {
+                perror("fopen");
+                ret = EXIT_FAILURE;
+                continue;
+            }
+
+            fprintf(fl, "%d %d\n", w.ws_xpixel, w.ws_ypixel);
+            fclose(fl);
+        }
+    }
+
+    return ret;
 }
