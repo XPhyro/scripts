c48a2c2c97f161a15b840b21f6caeb42826e6c11
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2024-01-10 13:45:05 +0300

    Create cpp/include/functionalutil.hpp

---
+++ src/cpp/include/functionalutil.hpp
@@ -0,0 +1,31 @@
+#ifndef HEADER_SCRIPTS_CXX_FUNCTIONALUTIL_
+#define HEADER_SCRIPTS_CXX_FUNCTIONALUTIL_
+
+#include <functional>
+
+namespace xph {
+    template <class Tret, class... Tparams>
+    inline constexpr void* address_of(const std::function<Tret(Tparams...)>& func);
+
+    template <class Tret, class... Tparams>
+    inline constexpr void* address_of(const std::function<Tret(Tparams...)>& func)
+    {
+        using func_t = Tret(Tparams...);
+        func_t** func_ptr = func.template target<func_t*>();
+        return reinterpret_cast<void*>(*func_ptr);
+    }
+
+    template <class Tret, class... Tparams, class Tother_ret, class... Tother_params>
+    inline constexpr bool
+    function_eq(const std::function<Tret(Tparams...)>& func,
+                const std::function<Tother_ret(Tother_params...)>& other_func);
+
+    template <class Tret, class... Tparams, class Tother_ret, class... Tother_params>
+    inline constexpr bool function_eq(const std::function<Tret(Tparams...)>& func,
+                                      const std::function<Tother_ret(Tother_params...)>& other_func)
+    {
+        return address_of(func) == address_of(other_func);
+    }
+} // namespace xph
+
+#endif /* ifndef HEADER_SCRIPTS_CXX_FUNCTIONALUTIL_ */
