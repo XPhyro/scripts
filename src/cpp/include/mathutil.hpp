94980469101da0d287b44b78148b468dc01d7bc8
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2024-01-14 22:50:16 +0300

    mathutil.hpp: support threshold overrides for approximate checks

--- src/cpp/include/mathutil.hpp
+++ src/cpp/include/mathutil.hpp
@@ -2,24 +2,24 @@
 #define HEADER_SCRIPTS_CXX_MATHUTIL_
 
 namespace xph {
-    inline bool approx_zero(float x)
+    inline bool approx_zero(float x, float e = 1e-6f)
     {
-        return -1e-6f < x && x < 1e-6f;
+        return -e < x && x < e;
     }
 
-    inline bool approx_zero(double x)
+    inline bool approx_zero(double x, double e = 1e-12)
     {
-        return -1e-12 < x && x < 1e-12;
+        return -e < x && x < e;
     }
 
-    inline bool approx_eq(float x, float y)
+    inline bool approx_eq(float x, float y, float e = 1e-6f)
     {
-        return approx_zero(x - y);
+        return approx_zero(x - y, e);
     }
 
-    inline bool approx_eq(double x, double y)
+    inline bool approx_eq(double x, double y, double e = 1e-12)
     {
-        return approx_zero(x - y);
+        return approx_zero(x - y, e);
     }
 } // namespace xph
 
