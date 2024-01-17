ef01ee3ad4337776c7af429fd490b07712e5d8ba
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2024-01-17 12:15:32 +0300

    sudo.sh: use alternative syntax due to shellcheck error
    The current syntax is valid, but shellcheck (intentionally) cannot parse it.

--- src/sh/include/sudo.sh
+++ src/sh/include/sudo.sh
@@ -4,13 +4,21 @@
     . meta.sh
 
     if [ "$(id -u)" = 0 ]; then
-        __sudo_func() "$@"
+        __sudo_func() {
+            "$@"
+        }
     elif std_has_command doas; then
-        __sudo_func() doas -- "$@"
+        __sudo_func() {
+            doas -- "$@"
+        }
     elif std_has_command sudo; then
-        __sudo_func() sudo -- "$@"
+        __sudo_func() {
+            sudo -- "$@"
+        }
     else
-        __sudo_func() "$@"
+        __sudo_func() {
+            "$@"
+        }
     fi
 
     std_sudo() {
