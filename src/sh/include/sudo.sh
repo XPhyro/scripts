801a2609827628f793e7d07ba69c96f30456c279
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2024-01-17 12:11:04 +0300

    sudo.sh: refactor

--- src/sh/include/sudo.sh
+++ src/sh/include/sudo.sh
@@ -4,16 +4,16 @@
     . meta.sh
 
     if [ "$(id -u)" = 0 ]; then
-        unset __sudo_cmd
+        __sudo_func() "$@"
     elif std_has_command doas; then
-        __sudo_cmd="doas --"
+        __sudo_func() doas -- "$@"
     elif std_has_command sudo; then
-        __sudo_cmd="sudo --"
+        __sudo_func() sudo -- "$@"
     else
-        unset __sudo_cmd
+        __sudo_func() "$@"
     fi
 
     std_sudo() {
-        $__sudo_cmd "$@"
+        __sudo_func "$@"
     }
 }
