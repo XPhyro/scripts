23520f23b7c74cb1ca521b63ab8779f16712172c
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2024-01-18 20:56:58 +0300

    Create c/include/exec_info.h

---
+++ src/c/include/exec_info.h
@@ -0,0 +1,27 @@
+#ifndef HEADER_SCRIPTS_C_EXECINFO_
+#define HEADER_SCRIPTS_C_EXECINFO_
+
+#include <stdio.h>
+#include <string.h>
+
+#define DEFINE_EXEC_INFO() \
+    const char *execname;  \
+    const char *execpath;
+
+extern const char *execname;
+extern const char *execpath;
+
+void init_exec_info(int argc, char **argv)
+{
+    char *s;
+
+    if (argc) {
+        execpath = argv[0];
+        execname = (s = strrchr(execpath, '/')) ? s + 1 : execpath;
+    } else {
+        execname = "NULL";
+        execpath = "NULL";
+    }
+}
+
+#endif /* ifndef HEADER_SCRIPTS_C_EXECINFO_ */
