9a2b7baa183431b38a6da64c5808ce5393441064
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2024-01-18 11:06:16 +0300

    tabutils: add support for single-tab copying & inline pasting

--- src/js/firefox/tabutils/background.js
+++ src/js/firefox/tabutils/background.js
@@ -3,6 +3,10 @@ chrome.commands.onCommand.addListener(function(command) {
         copyTabs();
     } else if (command === "pasteTabs") {
         pasteTabs();
+    } else if (command === "copyCurrentTab") {
+        copyCurrentTab();
+    } else if (command === "pasteNextTab") {
+        pasteNextTab();
     }
 });
 
@@ -20,7 +24,29 @@ function pasteTabs()
         let urls = clipboardData.split('\n');
         urls.forEach(url => {
             if (url.trim() !== '') {
-                chrome.tabs.create({ url: url });
+                chrome.tabs.create({ url: url, active: true });
+            }
+        });
+    });
+}
+
+function copyCurrentTab()
+{
+    chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+        let tabInfo = `${tabs[0].title}: ${tabs[0].url}`;
+        writeToClipboard(tabInfo);
+    });
+}
+
+function pasteNextTab()
+{
+    readFromClipboard(function(clipboardData) {
+        let urls = clipboardData.split('\n');
+        chrome.tabs.query({ currentWindow: true, active: true }, function(currentTabs) {
+            let currentTab = currentTabs[0];
+            let currentIndex = currentTab.index;
+            for (let i = 0; i < urls.length; i++) {
+                chrome.tabs.create({ url: urls[i], index: currentIndex + i + 1, active: true });
             }
         });
     });
