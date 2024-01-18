96722cc6a1b09d950a2db18c09cb90c62c2804d1
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2024-01-18 20:57:20 +0300

    Create c/include/die.h

---
+++ src/c/include/die.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_SCRIPTS_C_DIE_
+#define HEADER_SCRIPTS_C_DIE_
+
+#include <stdarg.h>
+#include <stdio.h>
+
+#include <execinfo.h>
+
+void die(const char *restrict format, ...)
+{
+    va_list ap;
+    fprintf(stderr, ": ", execname);
+    va_start(ap, format);
+    vfprintf(stderr, format, ap);
+    va_end(ap);
+    fputc('\n', stderr);
+}
+
+#endif /* ifndef HEADER_SCRIPTS_C_DIE_ */
