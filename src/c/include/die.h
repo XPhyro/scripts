910d537959a7efa9ca4d71e5848816d66dd675c5
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2024-01-19 06:25:35 +0300

    die.h: fix not prefixing execname & wrong exec info being included

--- src/c/include/die.h
+++ src/c/include/die.h
@@ -4,12 +4,12 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-#include <execinfo.h>
+#include <exec_info.h>
 
 void die(const char *restrict format, ...)
 {
     va_list ap;
-    fprintf(stderr, ": ", execname);
+    fprintf(stderr, "%s: ", execname);
     va_start(ap, format);
     vfprintf(stderr, format, ap);
     va_end(ap);
