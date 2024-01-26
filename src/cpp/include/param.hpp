97befd82977036f1c92b5993fc037bcf52500c4a
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2024-01-26 18:56:41 +0300

    Create cpp/include/param.hpp

---
+++ src/cpp/include/param.hpp
@@ -0,0 +1,68 @@
+#ifndef HEADER_SCRIPTS_CXX_PARAM_
+#define HEADER_SCRIPTS_CXX_PARAM_
+
+namespace xph {
+    template <typename Treturn, typename... Ts>
+    inline constexpr Treturn coalesce(const Ts&... args)
+    {
+        Treturn ret{};
+
+        ([&]() {
+            bool condition = static_cast<bool>(args);
+            if (condition)
+                ret = static_cast<Treturn>(args);
+            return condition;
+        }() ||
+         ...);
+
+        return ret;
+    }
+
+    template <typename Treturn, typename... Ts>
+    inline constexpr Treturn reverse_coalesce(const Ts&... args)
+    {
+        Treturn ret{};
+
+        (... || [&]() {
+            bool condition = static_cast<bool>(args);
+            if (condition)
+                ret = static_cast<Treturn>(args);
+            return condition;
+        }());
+
+        return ret;
+    }
+
+    template <typename Treturn, typename... Ts>
+    inline constexpr Treturn coalesce_deref(const Ts&... args)
+    {
+        Treturn ret{};
+
+        ([&]() {
+            bool condition = static_cast<bool>(args);
+            if (condition)
+                ret = static_cast<Treturn>(*args);
+            return condition;
+        }() ||
+         ...);
+
+        return ret;
+    }
+
+    template <typename Treturn, typename... Ts>
+    inline constexpr Treturn reverse_coalesce_deref(const Ts&... args)
+    {
+        Treturn ret{};
+
+        (... || [&]() {
+            bool condition = static_cast<bool>(args);
+            if (condition)
+                ret = static_cast<Treturn>(*args);
+            return condition;
+        }());
+
+        return ret;
+    }
+} // namespace xph
+
+#endif /* ifndef HEADER_SCRIPTS_CXX_PARAM_ */
