5a20113a53e51ce5861a7b7e098eee5a4072adf2
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2024-01-26 19:12:30 +0300

    param.hpp: add coalesce variants

--- src/cpp/include/param.hpp
+++ src/cpp/include/param.hpp
@@ -1,6 +1,8 @@
 #ifndef HEADER_SCRIPTS_CXX_PARAM_
 #define HEADER_SCRIPTS_CXX_PARAM_
 
+#include <string_view>
+
 namespace xph {
     template <typename Treturn, typename... Ts>
     inline constexpr Treturn coalesce(const Ts&... args)
@@ -58,6 +60,68 @@ namespace xph {
             bool condition = static_cast<bool>(args);
             if (condition)
                 ret = static_cast<Treturn>(*args);
+            return condition;
+        }());
+
+        return ret;
+    }
+
+    template <typename Treturn, typename... Ts>
+    inline constexpr Treturn coalesce_map(const auto& callable, const Ts&... args)
+    {
+        Treturn ret{};
+
+        ([&]() {
+            bool condition = static_cast<bool>(args);
+            if (condition)
+                ret = static_cast<Treturn>(callable(args));
+            return condition;
+        }() ||
+         ...);
+
+        return ret;
+    }
+
+    template <typename Treturn, typename... Ts>
+    inline constexpr Treturn reverse_coalesce_map(const auto& callable, const Ts&... args)
+    {
+        Treturn ret{};
+
+        (... || [&]() {
+            bool condition = static_cast<bool>(args);
+            if (condition)
+                ret = static_cast<Treturn>(callable(args));
+            return condition;
+        }());
+
+        return ret;
+    }
+
+    template <typename Treturn, typename... Ts>
+    inline constexpr Treturn coalesce_deref_map(const auto& callable, const Ts&... args)
+    {
+        Treturn ret{};
+
+        ([&]() {
+            bool condition = static_cast<bool>(args);
+            if (condition)
+                ret = static_cast<Treturn>(callable(*args));
+            return condition;
+        }() ||
+         ...);
+
+        return ret;
+    }
+
+    template <typename Treturn, typename... Ts>
+    inline constexpr Treturn reverse_coalesce_deref_map(const auto& callable, const Ts&... args)
+    {
+        Treturn ret{};
+
+        (... || [&]() {
+            bool condition = static_cast<bool>(args);
+            if (condition)
+                ret = static_cast<Treturn>(callable(*args));
             return condition;
         }());
 
