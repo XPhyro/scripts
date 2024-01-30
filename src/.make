XPhyro/scripts

commit 187fe47bdef418016f738d14162eddd3b90be4e4

.make#spell: simplify message, disable encoding detection, simplify word list, ignore resources and test outputs

src/.make
@@ -903,18 +903,20 @@ spell() {
         cd .. || exit 1
 
         printf "%s\n" \
-            "Checking files and files in directories for spelling mistakes:"
-        find '.' -type f -not -path "*/.git/*" \
+            "Checking for typos:"
+        find '.' -type f -not -name "tags" \
+                         -not -path "*/.git/*" \
+                         -not -path "*/rsrc/*" \
                          -not -path "*/lib/*" \
                          -not -path "*/.archived/*" \
                          -not -path "*/rs/*/target/*" \
-                         -not -name "tags" \
+                         -not -path "*/src/tests/*/out" \
             -print0 \
+            | sort -z \
             | xargs -r0 \
                 codespell \
-                    -e \
                     --builtin "clear,rare,informal" \
-                    -L "ans,ba,erformance,crate,doas,ond,hist" \
+                    -L "ans,erformance,crate,ond" \
                     --
     )
 }

commit b1d136f86e35638b47199fc763668308556da05c

.make#spell: fix regression affecting CI
Fixed regression introduced in 187fe47bdef41 affecting CI that uses older version of codespell from Ubuntu repositories.

src/.make
@@ -916,7 +916,7 @@ spell() {
             | xargs -r0 \
                 codespell \
                     --builtin "clear,rare,informal" \
-                    -L "ans,erformance,crate,ond" \
+                    -L "ans,ba,crate,doas,erformance,hist,ond" \
                     --
     )
 }