7d2501e8862f45240ab5a9f8f464b6d5a4d3723c
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2024-02-07 16:44:23 +0300

    type.sh: add std_is_{,{,un}signed_}number

--- src/sh/util/include/type.sh
+++ src/sh/util/include/type.sh
@@ -7,4 +7,25 @@
         done
         return 0
     }
+
+    std_is_number() {
+        for __i; do
+            printf "%s\n" "$__i" | grep -Exq '[-+]?([0-9]+|[0-9]+\.|\.[0-9]+|[0-9]+\.[0-9]+)' || return 1
+        done
+        return 0
+    }
+
+    std_is_unsigned_number() {
+        for __i; do
+            printf "%s\n" "$__i" | grep -Exq '([0-9]+|[0-9]+\.|\.[0-9]+|[0-9]+\.[0-9]+)' || return 1
+        done
+        return 0
+    }
+
+    std_is_signed_number() {
+        for __i; do
+            printf "%s\n" "$__i" | grep -Exq '[-+]([0-9]+|[0-9]+\.|\.[0-9]+|[0-9]+\.[0-9]+)' || return 1
+        done
+        return 0
+    }
 }
