0d7f82ba615d3e2a4c32e6276ad719c47b89e164
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2024-02-26 11:51:12 +0300

    open-youtube-thumbnail: refactor

--- src/js/browser/open-youtube-thumbnail/background.js
+++ src/js/browser/open-youtube-thumbnail/background.js
@@ -1,13 +1,10 @@
-chrome.browserAction.onClicked.addListener((tab) => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        const currentTab = tabs[0];
-        const videoIdMatch = currentTab.url.match(/youtube\.com\/.*[?&]v=([^&]+)/);
+chrome.browserAction.onClicked.addListener((currentTab) => {
+    const videoIdMatch = currentTab.url.match(/youtube\.com\/.*[?&]v=([^&]+)/);
 
-        if (videoIdMatch) {
-            const videoId = videoIdMatch[1];
-            const thumbnailURL = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
+    if (videoIdMatch) {
+        const videoId = videoIdMatch[1];
+        const thumbnailURL = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
 
-            chrome.tabs.create({ url: thumbnailURL, index: currentTab.index + 1 });
-        }
-    });
+        chrome.tabs.create({ url: thumbnailURL, index: currentTab.index + 1 });
+    }
 });
