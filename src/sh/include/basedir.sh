5141a2b82d7a60c7bddf28f854ae5917d7188272
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2024-04-06 12:59:01 +0300

    sh/*: rename config.sh to basedir.sh

--- src/sh/include/basedir.sh
+++ src/sh/include/basedir.sh
@@ -12,10 +12,10 @@
     std_config_home="$std_global_config_home/$std_execname"
 
     [ -d "$std_xdg_config_home" ] || mkdir -- "$std_xdg_config_home" \
-        || std_logerr "config.sh: system does not obey XDG Base Directory Specification"
+        || std_logerr "basedir.sh: system does not obey XDG Base Directory Specification"
     [ -d "$std_global_config_home" ] || mkdir -- "$std_global_config_home" \
-        || std_logerr "config.sh: cannot set up global config directory"
+        || std_logerr "basedir.sh: cannot set up global config directory"
     # assumption: if the script includes this header, it wants/needs a config sub-directory
     [ -d "$std_config_home" ] || mkdir -- "$std_config_home" \
-        || std_logerr "config.sh: cannot set up script config directory"
+        || std_logerr "basedir.sh: cannot set up script config directory"
 }
