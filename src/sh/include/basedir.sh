baae8d81f0a5350341acf3892a4506e3ff433488
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2024-04-06 13:06:10 +0300

    basedir.sh: add cache directories

--- src/sh/include/basedir.sh
+++ src/sh/include/basedir.sh
@@ -1,5 +1,7 @@
 . std.sh
 
+# shellcheck disable=SC2154
+
 [ -z "${HEADER_SCRIPTS_SH_CONFIG_+x}" ] && {
     HEADER_SCRIPTS_SH_CONFIG_=
 
@@ -7,15 +9,23 @@
     . io.sh
 
     std_xdg_config_home="${XDG_CONFIG_HOME:-"$HOME/.config"}"
-    std_global_config_home="$std_xdg_config_home/scripts"
-    # shellcheck disable=SC2154
-    std_config_home="$std_global_config_home/$std_execname"
+    std_xdg_cache_home="${XDG_CACHE_HOME:-"$HOME/.cache"}"
 
-    [ -d "$std_xdg_config_home" ] || mkdir -- "$std_xdg_config_home" \
-        || std_logerr "basedir.sh: system does not obey XDG Base Directory Specification"
-    [ -d "$std_global_config_home" ] || mkdir -- "$std_global_config_home" \
-        || std_logerr "basedir.sh: cannot set up global config directory"
-    # assumption: if the script includes this header, it wants/needs a config sub-directory
-    [ -d "$std_config_home" ] || mkdir -- "$std_config_home" \
-        || std_logerr "basedir.sh: cannot set up script config directory"
+    std_global_config_home="$std_xdg_config_home/scripts"
+    std_global_cache_home="$std_xdg_cache_home/scripts"
+
+    std_config_home="$std_global_config_home/$std_execname"
+    std_cache_home="$std_global_cache_home/$std_execname"
+
+    { [ -d "$std_xdg_config_home" ] || mkdir -- "$std_xdg_config_home"; } \
+        || { [ -d "$std_xdg_cache_home" ] || mkdir -- "$std_xdg_cache_home"; } \
+        || std_logerr "basedir.sh: system does not conform to XDG Base Directory Specification"
+
+    { [ -d "$std_global_config_home" ] || mkdir -- "$std_global_config_home"; } \
+        || { [ -d "$std_global_cache_home" ] || mkdir -- "$std_global_cache_home"; } \
+        || std_logerr "basedir.sh: cannot set up global directories"
+
+    { [ -d "$std_config_home" ] || mkdir -- "$std_config_home"; } \
+        || { [ -d "$std_cache_home" ] || mkdir -- "$std_cache_home"; } \
+        || std_logerr "basedir.sh: cannot set up script directories"
 }
