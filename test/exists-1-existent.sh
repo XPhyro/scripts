9fd42aa4450fdd532650eb18c06f4b01408e87ba
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2024-04-23 16:09:22 +0300

    test/exists-*: create

---
+++ test/exists-1-existent.sh
@@ -0,0 +1,5 @@
+test_binary=exists
+test_name="Existent"
+test_exit_code=0
+
+set -- "$realexecpath"
---
+++ test/exists-1-non-existent.sh
@@ -0,0 +1,5 @@
+test_binary=exists
+test_name="Non-Existent"
+test_exit_code=1
+
+set -- "$realexecpath"/
