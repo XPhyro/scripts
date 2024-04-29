18bf5a636b62c0ba74f69989306192b719e4ba4b
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2024-04-29 04:02:58 +0300

    type.sh: add assert variants

--- src/sh/include/type.sh
+++ src/sh/include/type.sh
@@ -10,11 +10,23 @@
         return 0
     }
 
+    std_assert_digit() {
+        for __j; do
+            std_is_digit "$__j" || std_logerrq "expected digit, got '$__j'"
+        done
+    }
+
     std_is_number() {
         for __i; do
             printf "%s\n" "$__i" | grep -Exq '[-+]?([0-9]+|[0-9]+\.|\.[0-9]+|[0-9]+\.[0-9]+)' || return 1
         done
         return 0
+    }
+
+    std_assert_number() {
+        for __j; do
+            std_is_number "$__j" || std_logerrq "expected number, got '$__j'"
+        done
     }
 
     std_is_unsigned_number() {
@@ -24,10 +36,22 @@
         return 0
     }
 
+    std_assert_unsigned_number() {
+        for __j; do
+            std_is_unsigned_number "$__j" || std_logerrq "expected unsigned number, got '$__j'"
+        done
+    }
+
     std_is_signed_number() {
         for __i; do
             printf "%s\n" "$__i" | grep -Exq '[-+]([0-9]+|[0-9]+\.|\.[0-9]+|[0-9]+\.[0-9]+)' || return 1
         done
         return 0
+    }
+
+    std_assert_signed_number() {
+        for __j; do
+            std_is_signed_number "$__j" || std_logerrq "expected signed number, got '$__j'"
+        done
     }
 }
