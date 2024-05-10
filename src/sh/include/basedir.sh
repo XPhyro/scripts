ee944efcb8ce5e9291d27bece147a7607da45c0c
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2024-05-10 08:35:27 +0300

    basedir.sh: fix setting up directories

--- src/sh/include/basedir.sh
+++ src/sh/include/basedir.sh
@@ -18,14 +18,14 @@
     std_cache_home="$std_global_cache_home/$std_execname"
 
     { [ -d "$std_xdg_config_home" ] || mkdir -- "$std_xdg_config_home"; } \
-        || { [ -d "$std_xdg_cache_home" ] || mkdir -- "$std_xdg_cache_home"; } \
+        && { [ -d "$std_xdg_cache_home" ] || mkdir -- "$std_xdg_cache_home"; } \
         || std_logerr "basedir.sh: system does not conform to XDG Base Directory Specification"
 
     { [ -d "$std_global_config_home" ] || mkdir -- "$std_global_config_home"; } \
-        || { [ -d "$std_global_cache_home" ] || mkdir -- "$std_global_cache_home"; } \
+        && { [ -d "$std_global_cache_home" ] || mkdir -- "$std_global_cache_home"; } \
         || std_logerr "basedir.sh: cannot set up global directories"
 
     { [ -d "$std_config_home" ] || mkdir -- "$std_config_home"; } \
-        || { [ -d "$std_cache_home" ] || mkdir -- "$std_cache_home"; } \
+        && { [ -d "$std_cache_home" ] || mkdir -- "$std_cache_home"; } \
         || std_logerr "basedir.sh: cannot set up script directories"
 }
