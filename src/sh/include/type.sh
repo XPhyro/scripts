67492bf92ad98bfcf16bd2957676cb11c7b56f4f
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2024-07-03 22:23:28 +0300

    type.sh: fix dependency not being included

--- src/sh/include/type.sh
+++ src/sh/include/type.sh
@@ -3,6 +3,8 @@
 [ -z "${HEADER_SCRIPTS_SH_TYPE_+x}" ] && {
     HEADER_SCRIPTS_SH_TYPE_=
 
+    . io.sh
+
     std_is_digit() {
         for __i; do
             [ "$__i" -eq "$__i" ] 2> /dev/null || return 1
