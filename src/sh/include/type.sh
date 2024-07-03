1a4d09f938571a870410d09fe373a2483a17aa93
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2024-07-03 23:01:28 +0300

    type.sh: add `std_is_number_in_range` and `std_assert_number_in_range`

--- src/sh/include/type.sh
+++ src/sh/include/type.sh
@@ -56,4 +56,30 @@
             std_is_signed_number "$__j" || std_logerrq "expected signed number, got '$__j'"
         done
     }
+
+    std_is_number_in_range() {
+        while [ "$#" -ne 0 ]; do
+            __i="$1"
+            __i_min="$2"
+            __i_max="$3" 
+            shift 3 || return 1
+            std_is_number "$__i" \
+                && std_is_number "$__i_min" \
+                && std_is_number "$__i_max" \
+                || return 1
+            [ "$(printf "scale=15; %s <= %s && %s <= %s\n" "$__i_min" "$__i" "$__i" "$__i_max" | bc 2> /dev/null)" = 1 ] || return 1
+        done
+        return 0
+    }
+
+    std_assert_number_in_range() {
+        while [ "$#" -ne 0 ]; do
+            __j="$1"
+            __j_min="$2"
+            __j_max="$3" 
+            shift 3 || return 1
+            std_is_number_in_range "$__j" "$__j_min" "$__j_max" \
+                || std_logerrq "expected number in range [$__j_min, $__j_max], got '$__j'"
+        done
+    }
 }
