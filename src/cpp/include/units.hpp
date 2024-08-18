c818b52d75c86dfdd549987b6130bbf77a47636b
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2024-08-18 04:25:17 +0300

    units.hpp: use std::array

--- src/cpp/include/units.hpp
+++ src/cpp/include/units.hpp
@@ -1,17 +1,16 @@
 #ifndef HEADER_SCRIPTS_CXX_UNITS_
 #define HEADER_SCRIPTS_CXX_UNITS_
 
+#include <array>
 #include <string>
 
 namespace xph::unit {
     inline std::string tick_to_human(std::size_t tick)
     {
-        enum { unit_count = 10 };
-
-        static const std::string units[unit_count] = {
+        static const std::array<std::string, 10> units = {
             "y", "mo", "w", "d", "h", "m", "s", "ms", "us", "ns",
         };
-        static const std::size_t durations[unit_count] = {
+        static const std::array<std::size_t, units.size()> durations = {
             31'556'952'000'000'000uz,
             2'629'746'000'000'000uz,
             604'800'000'000'000uz,
@@ -28,7 +27,7 @@ namespace xph::unit {
             return "0s";
 
         std::string s = "";
-        for (int i = 0; i < unit_count; ++i) {
+        for (auto i = 0uz; i < units.size(); ++i) {
             if (tick >= durations[i]) {
                 s += std::to_string(tick / durations[i]) + units[i];
                 tick %= durations[i];
