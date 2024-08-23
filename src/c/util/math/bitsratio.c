17bbc90a6601a1e4f40f4d572d8a66c671263508
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2024-08-23 12:19:17 +0300

    {bitsratio,popcount}: use `popcountu` from math.h

--- src/c/util/math/bitsratio.c
+++ src/c/util/math/bitsratio.c
@@ -3,31 +3,19 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include <xph/math.h>
+
 int main(int argc, char *argv[])
 {
     ssize_t nread, i;
     size_t totread, popcount;
     char buf[PIPE_BUF];
-#if defined(__GNUC__) || defined(__clang__)
-    unsigned int c;
-#else
-    char c;
-#endif
 
     popcount = 0;
     totread = 0;
     while ((nread = read(STDIN_FILENO, buf, PIPE_BUF)) > 0) {
         totread += nread * 8;
-        for (i = 0; i < nread; i++) {
-            c = buf[i];
-#if defined(__GNUC__) || defined(__clang__)
-            popcount += __builtin_popcount(c & 0xff);
-#else
-            popcount += ((c & (1 << 0)) >> 0) + ((c & (1 << 1)) >> 1) + ((c & (1 << 2)) >> 2) +
-                        ((c & (1 << 3)) >> 3) + ((c & (1 << 4)) >> 4) + ((c & (1 << 5)) >> 5) +
-                        ((c & (1 << 6)) >> 6) + ((c & (1 << 7)) >> 7);
-#endif
-        }
+        for (i = 0; i < nread; popcount += popcountu(buf[i++] & 0xff)) {}
     }
 
     printf("%f\n", popcount / (double)totread);
--- src/c/util/math/popcount.c
+++ src/c/util/math/popcount.c
@@ -3,34 +3,17 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#if !defined(USE_BUILTIN) && (defined(__GNUC__) || defined(__clang__))
-#define USE_BUILTIN
-#endif
+#include <xph/math.h>
 
 int main(int argc, char *argv[])
 {
     ssize_t nread, i;
     size_t popcount;
     char buf[PIPE_BUF];
-#ifdef USE_BUILTIN
-    unsigned int c;
-#else
-    char c;
-#endif
 
     popcount = 0;
-    while ((nread = read(STDIN_FILENO, buf, PIPE_BUF)) > 0) {
-        for (i = 0; i < nread; i++) {
-            c = buf[i];
-#ifdef USE_BUILTIN
-            popcount += __builtin_popcount(c & 0xff);
-#else
-            popcount += ((c & (1 << 0)) >> 0) + ((c & (1 << 1)) >> 1) + ((c & (1 << 2)) >> 2) +
-                        ((c & (1 << 3)) >> 3) + ((c & (1 << 4)) >> 4) + ((c & (1 << 5)) >> 5) +
-                        ((c & (1 << 6)) >> 6) + ((c & (1 << 7)) >> 7);
-#endif
-        }
-    }
+    while ((nread = read(STDIN_FILENO, buf, PIPE_BUF)) > 0)
+        for (i = 0; i < nread; popcount += popcountu(buf[i++] & 0xff)) {}
 
     printf("%zu\n", popcount);
 
